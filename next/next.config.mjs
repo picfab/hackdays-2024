799287a65dad80631208377511676510301cc4d0
Fabien Picard <pic.fab@hotmail.fr>
2024-06-13 00:27:07 +0200

    wip

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: 'http',
