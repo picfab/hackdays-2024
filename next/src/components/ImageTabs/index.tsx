fd8ddf9e8ff15259cfec5d52bc68e2b27c431b1c
Fabien Picard <pic.fab@hotmail.fr>
2024-06-13 01:00:02 +0200

    wip

--- next/src/components/ImageTabs/index.tsx
+++ next/src/components/ImageTabs/index.tsx
@@ -1,6 +1,7 @@
 import { prepareImageData } from '@/utils/image';
 import { prepareRepeaterData } from '@/utils/prepareRepeaterData';
 import { ImageTabsComponent } from './ImageTab';
+import { classNames } from '@/utils/classNames';
 
 export const ImageTabs = async (props: any) => {
   const _tabs = prepareRepeaterData('tabs', props);
@@ -12,5 +13,24 @@ export const ImageTabs = async (props: any) => {
     })
   );
 
-  return <ImageTabsComponent {...props} tabs={tabs} />;
+  return (
+    <div
+      className={classNames(
+        'SectionImageTabs',
+        // container,
+        'text-neutral-301 flex',
+        'flex-col items-center'
+      )}
+    >
+      <h2
+        className={classNames(
+          'mb-64 font-medium text-center md:w-6/12'
+          // textTitle1
+        )}
+      >
+        {props?.title}
+      </h2>
+      <ImageTabsComponent {...props} tabs={tabs} />
+    </div>
+  );
 };
