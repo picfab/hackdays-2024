2ab0eea7f2ba88469aea225f702467ef1545ec90
Ubuntu <ubuntu@ip-172-31-0-172.eu-west-3.compute.internal>
2024-06-12 11:34:31 +0000

    Fix certbot

--- certbot.sh
+++ certbot.sh
@@ -3,15 +3,15 @@
 if [ "$1" == "renew" ]
 then
   docker compose exec nginx certbot -n --nginx --agree-tos \
-    -d hackday.reiwilde.me \
-    -d hackday-db.reiwilde.me \
-    -d hackday-wp.reiwilde.me \
+    -d hackdays.reiwilde.me \
+    -d hackdays-db.reiwilde.me \
+    -d hackdays-wp.reiwilde.me \
     $@
 elif [ "$1" == "create" ]
 then
   docker compose exec nginx certbot -n --nginx --agree-tos \
     -m certbot@reiwilde.com \
-    -d hackday.reiwilde.me \
-    -d hackday-db.reiwilde.me \
-    -d hackday-wp.reiwilde.me
+    -d hackdays.reiwilde.me \
+    -d hackdays-db.reiwilde.me \
+    -d hackdays-wp.reiwilde.me
 fi
