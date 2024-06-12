0bf759435181132aec3e1fbfcd64752448f8ff12
Ubuntu <ubuntu@ip-172-31-0-172.eu-west-3.compute.internal>
2024-06-12 15:03:32 +0000

    Deploy ready

--- certbot.sh
+++ certbot.sh
@@ -3,15 +3,15 @@
 if [ "$1" == "renew" ]
 then
   docker compose exec nginx certbot -n --nginx --agree-tos \
-    -d hackdays.reiwilde.me \
-    -d hackdays-db.reiwilde.me \
-    -d hackdays-wp.reiwilde.me \
+    -d hackdays.reiwilde.com \
+    -d hackdays-db.reiwilde.com \
+    -d hackdays-wp.reiwilde.com \
     $@
 elif [ "$1" == "create" ]
 then
   docker compose exec nginx certbot -n --nginx --agree-tos \
     -m certbot@reiwilde.com \
-    -d hackdays.reiwilde.me \
-    -d hackdays-db.reiwilde.me \
-    -d hackdays-wp.reiwilde.me
+    -d hackdays.reiwilde.com \
+    -d hackdays-db.reiwilde.com \
+    -d hackdays-wp.reiwilde.com
 fi
