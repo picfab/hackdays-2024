0bf21f6462839810863f0df5d8545db2206843ab
Fabien Picard <pic.fab@hotmail.fr>
2024-06-14 00:45:11 +0200

    wip

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,11 +1,24 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '*./svg',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
         hostname: process.env.NEXT_PUBLIC_WORDPRESS_HOSTNAME,
-	pathname: process.env.NEXT_PUBLIC_WORPRESS_CONTENT_PATH,
+        pathname: process.env.NEXT_PUBLIC_WORPRESS_CONTENT_PATH,
         port: process.env.NEXT_PUBLIC_WORDPRESS_PORT,
         protocol: process.env.NEXT_PUBLIC_WORDPRESS_PROTOCOL,
       },
