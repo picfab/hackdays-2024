b1d681b5e92f24a8c940b2b64426f0873128d816
Ubuntu <ubuntu@ip-172-31-0-172.eu-west-3.compute.internal>
2024-06-13 11:08:47 +0000

    feat(next): image opti

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: process.env.NEXT_PUBLIC_WORDPRESS_PROTOCOL,
         hostname: process.env.NEXT_PUBLIC_WORDPRESS_HOSTNAME,
+	pathname: process.env.NEXT_PUBLIC_WORPRESS_CONTENT_PATH,
         port: process.env.NEXT_PUBLIC_WORDPRESS_PORT,
+        protocol: process.env.NEXT_PUBLIC_WORDPRESS_PROTOCOL,
       },
     ],
   },
