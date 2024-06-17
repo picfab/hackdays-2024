2577a398fa38535331597f979c11a2127fd00827
Fabien Picard <pic.fab@hotmail.fr>
2024-06-17 09:55:30 +0200

    update api

--- wordpress/wp-content/plugins/blocks-to-api/blocks-to-api.php
+++ wordpress/wp-content/plugins/blocks-to-api/blocks-to-api.php
@@ -39,6 +39,112 @@ add_action(
     }
 );
 
+
+function remove_leading_underscore_from_string($string)
+{
+    // Vérifiez si le premier caractère est un underscore
+    if (strpos($string, '_') === 0) {
+        // Supprimez le premier caractère
+        return substr($string, 1);
+    }
+    return $string;
+}
+
+function str_replace_first($search, $replace, $subject)
+{
+    $pos = strpos($subject, $search);
+    if ($pos !== false) {
+        $subject = substr_replace($subject, $replace, $pos, strlen($search));
+    }
+    return $subject;
+}
+
+function prepare_repeater_acf_field($acf_blocks)
+{
+    foreach ($acf_blocks as $key => $block) {
+        if ($block['type'] === 'repeater' && $block['value'] > 0) {
+            $nb_field = $block['value'];
+
+            $repeater_fields = array();
+            for ($i = 0; $i < $nb_field; $i++) {
+                foreach ($acf_blocks as $_key => $_value) {
+                    if (strpos($_key, $key . '_' . $i) === 0) {
+                        $clean_name = str_replace_first($key . '_' . $i . '_', '', $_key);
+                        $repeater_fields[$i][$clean_name] = $_value;
+                        unset($acf_blocks[$_key]);
+                    }
+                }
+            }
+
+
+            $acf_blocks[$key]['value'] = $repeater_fields;
+
+
+            // foreach ($acf_blocks[$key]['value'] as $_key => $value) {
+            //     foreach ($value as $__key => $__value) {
+            //         if ($__value['type'] === 'group') {
+            //             $acf_blocks[$key]['value'][] = prepare_group_acf_field($value);
+            //         }
+            //     }
+            // }
+        }
+
+        if (in_array($block['type'], ['group'])) {
+            // foreach ($block['value'] as $_key => $_value) {
+                // var_dump($_value);
+                $acf_blocks[$key]['value']  = prepare_repeater_acf_field($block['value']);
+            // }
+        }
+    }
+
+    return $acf_blocks;
+}
+
+function prepare_group_acf_field($acf_blocks)
+{
+    // var_dump($acf_blocks);
+    // echo '<hr>';
+    foreach ($acf_blocks as $key => $block) {
+        if ($block['type'] === 'group') {
+
+            $group_fields = array();
+            foreach ($acf_blocks as $_key => $_value) {
+                if (strpos($_key, $key . '_') === 0) {
+                    $clean_name = str_replace_first($key .  '_', '', $_key);
+                    $group_fields[$clean_name] = $_value;
+                    unset($acf_blocks[$_key]);
+                }
+            }
+
+            $acf_blocks[$key]['value'] = $group_fields;
+
+            // foreach ($acf_blocks[$key]['value'] as $_key => $value) {
+            //     // $acf_blocks[$key]['value'] = check_parent_field_acf($value, $acf_blocks[$key]['value']);
+            //     $acf_blocks[$key]['value'] = prepare_group_acf_field($acf_blocks[$key]['value']);
+
+            // }
+        }
+    }
+
+    return $acf_blocks;
+}
+
+function check_parent_field_acf($block, $acf_blocks)
+{
+    if ($block['type'] === 'group') {
+        // var_dump($acf_blocks, $block['name']);
+
+        $acf_blocks = prepare_group_acf_field($acf_blocks);
+    }
+    // var_dump($block);
+    // echo '<hr>';
+    // if ($block['type'] === 'repeater') {
+    //     $acf_blocks = prepare_repeater_acf_field($acf_blocks);
+    // }
+
+    return $acf_blocks;
+}
+
 add_action('rest_api_init', function () {
     register_rest_route('custom/v1', '/slug/(?P<slug>[a-zA-Z0-9-]+)', array(
         'methods' => 'GET',
@@ -49,6 +155,7 @@ add_action('rest_api_init', function () {
 
 function get_content_by_slug($data)
 {
+
     $args = array(
         'name' => $data['slug'],
         'post_type' => 'any', // Change 'any' to the desired post type if needed
@@ -93,8 +200,37 @@ function get_content_by_slug($data)
         $all_meta =  get_post_meta($post->ID);
 
 
+
+
+        $acf_blocks = array();
+        foreach ($blocks as $block) {
+            if (strpos($block['blockName'], 'payfit/') === 0) {
+                $data = $block['attrs']['data'];
+
+                foreach ($data as $key => $value) {
+                    if (strpos($key, '_') === 0) {
+                        $field_name = remove_leading_underscore_from_string($key);
+                        $acf_field = acf_get_field($value);
+                        $acf_blocks[$field_name] = array(
+                            'name' => $acf_field['name'],
+                            'label' => $acf_field['label'],
+                            'value' =>  $data[$field_name],
+                            'type' => $acf_field['type'],
+                            'acf_id' => $value,
+                        );
+                    }
+                }
+            }
+        }
+
+        // die(var_dump($acf_blocks));
+        $acf_blocks = prepare_group_acf_field($acf_blocks);
+        $acf_blocks = prepare_repeater_acf_field($acf_blocks);
+
+
         // Prepare response
         $response = array(
+            'acf_blocks' => $acf_blocks,
             'id' => $post_data->ID,
             'date' => $post_data->post_date,
             'slug' => $post_data->post_name,
